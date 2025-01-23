2104ff3b35327ba0f2485534af6da311b2f580fb
SADEK <sadek.contact@gmail.com>
2025-01-23 12:01:26 +0100

    Changer le sub OF

--- app/Model/Production.php
+++ app/Model/Production.php
@@ -15,7 +15,7 @@ class Production extends AppModel
 		parent::beforeSave($options);
 		if ( empty($this->data[$this->alias]['id']) AND empty( $this->data[$this->alias]['reference'] ) ){ 
 			$number = $this->find('count',['conditions'=>[$this->alias.'.deleted'=>[0,1]]]) + 1; 
-			$this->data[$this->alias]['reference'] = 'PRD-'.str_pad($number, 6, '0', STR_PAD_LEFT);
+			$this->data[$this->alias]['reference'] = 'OF-'.str_pad($number, 6, '0', STR_PAD_LEFT);
 		}
 		if (!empty($this->data[$this->alias]['date'])){
 	        $this->data[$this->alias]['date'] = $this->dateFormatBeforeSave( $this->data[$this->alias]['date'] );
